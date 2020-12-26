205056f6f9d8762523062abb98117d794ad595d6
didier <rodrigo.didier@luminae.com.br>
2020-12-26 00:05:49 -0300

    Signing up and writing Users to a database.

--- section5/code/user.py
+++ section5/code/user.py
@@ -1,4 +1,6 @@
+from flask_restful import Resource, reqparse
 import sqlite3
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
@@ -48,3 +50,28 @@ class User:
         connection.close()
         return user
 
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+            type=str,
+            required=True,
+            help="this fild cannot be blank.")
+
+    parser.add_argument('password',
+            type=str,
+            required=True,
+            help="this fild cannot be blank.")
+
+    def post(self):
+
+        data = UserRegister.parser.parse_args()
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'],data['password']))
+        
+        connection.commit()
+        connection.close()
+
+        return {"message":"User created succesfully."}, 201
