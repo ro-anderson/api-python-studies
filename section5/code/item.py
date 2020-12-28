313bd8a4ee5b33fbbb6b04b56cd2c3bed2876359
didier <rodrigo.didier@luminae.com.br>
2020-12-28 16:23:14 -0300

    (feat) add delete to api.

--- section5/code/item.py
+++ section5/code/item.py
@@ -14,34 +14,55 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'item not found.'}, 404
+    
+    @classmethod
+    def find_by_name(cls, name):
+
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         
         query = "SELECT * FROM items WHERE name=?"
         result = cursor.execute(query, (name,))
         row = result.fetchone()
-
         connection.close()
         if row:
             return {'item':{'name':row[0], 'price':row[1]}}
-        return {'message': 'item not found.'}, 404
 
 
     def post(self, name):
 
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if self.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         # data comes after leading with errors - Error first aproach
         data = Item.parser.parse_args()
-        data = request.get_json()
+
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
 
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name =?"
+
+        cursor.execute(query, (name,))
+        connection.commit()
+        connection.close()
         return {'message': 'Item deleted'}
 
     def put(self, name):
