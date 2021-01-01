da48673d4ce7f0976957f056e68cf140a7399401
didier <rodrigo.didier@luminae.com.br>
2021-01-01 15:52:36 -0300

    (feat and update) Creating User and Item models and update app code.

---
+++ sqlalchemy-restfull-app/code/models/user.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+class UserModel:
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        '''
+        by username retrieve User.
+        '''
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+
+        if row:
+            #user = cls(row[0], row[1], row[2])
+            user = cls(*row)  # *row = set of positional args
+
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        '''
+        by _id retrieve User.
+        '''
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+
+        if row:
+            #user = cls(row[0], row[1], row[2])
+            user = cls(*row)  # *row = set of positional args
+
+        else:
+            row = None
+
+        connection.close()
+        return user
+
--- sqlalchemy-restfull-app/code/resources/user.py
+++ sqlalchemy-restfull-app/code/resources/user.py
@@ -1,56 +1,6 @@
 from flask_restful import Resource, reqparse
 import sqlite3
-
-
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
-    @classmethod
-    def find_by_username(cls, username):
-        '''
-        by username retrieve User.
-        '''
-
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-
-        if row:
-            #user = cls(row[0], row[1], row[2])
-            user = cls(*row)  # *row = set of positional args
-
-        else:
-            user = None
-
-        connection.close()
-        return user
-
-    @classmethod
-    def find_by_id(cls, _id):
-        '''
-        by _id retrieve User.
-        '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-
-        if row:
-            #user = cls(row[0], row[1], row[2])
-            user = cls(*row)  # *row = set of positional args
-
-        else:
-            row = None
-
-        connection.close()
-        return user
-
+from models.user import UserModel
 
 class UserRegister(Resource):
 
@@ -69,7 +19,7 @@ class UserRegister(Resource):
 
         data = UserRegister.parser.parse_args()
 
-        if User.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {"message":"A user with that username already exists"}, 400
 
         connection = sqlite3.connect('data.db')
