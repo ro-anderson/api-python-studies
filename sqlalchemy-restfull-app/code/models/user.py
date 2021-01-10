5ecfe3e53fe37f298dd488ecdb09095b4222745a
didier <rodrigo.didier@luminae.com.br>
2021-01-10 15:32:31 -0300

    (feat) implement UserModel using SQLAlchemy

--- sqlalchemy-restfull-app/code/models/user.py
+++ sqlalchemy-restfull-app/code/models/user.py
@@ -13,51 +13,25 @@ class UserModel(db.Model):
     
 
 
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
     @classmethod
     def find_by_username(cls, username):
         '''
         by username retrieve User.
         '''
-
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-
-        if row:
-            #user = cls(row[0], row[1], row[2])
-            user = cls(*row)  # *row = set of positional args
-
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
         '''
         by _id retrieve User.
         '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-
-        if row:
-            #user = cls(row[0], row[1], row[2])
-            user = cls(*row)  # *row = set of positional args
-
-        else:
-            row = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first()
 
--- sqlalchemy-restfull-app/code/resources/user.py
+++ sqlalchemy-restfull-app/code/resources/user.py
@@ -21,13 +21,9 @@ class UserRegister(Resource):
 
         if UserModel.find_by_username(data['username']):
             return {"message":"A user with that username already exists"}, 400
-
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        
+        user = UserModel(**data) # instead of data['username'], data['password']
+                                     # for each keys in <dict> data, username = key, pass = value
+        user.save_to_db()
 
         return {"message": "User created succesfully."}, 201
